cf047b8d80dc5763f3d6c6cc09c51cdfc5aa5878
Kevin <kevin03.nguyen@gmail.com>
2022-01-06 15:15:04 -0500

    Change homepage

--- index.js
+++ index.js
@@ -5,7 +5,35 @@ const middleware = require('./utils/middleware');
 const newsRouter = require('./controllers/news');
 const PORT = process.env.PORT || 8000;
 
-app.get('', async (request, response) => response.send('NBA-API'));
+app.get('', async (request, response) =>
+  response.send(`<h1>NBA-Latest Stories-API</h1>
+<h2>About</h2>
+<div>Returns latest NBA articles based on teams or players from espn, bleacher report, nba.com, yahoo, and slam</div>
+
+<h2>Endpoints</h2> 
+
+<h3>/news/</h3>
+<p>Returns a list of all the latest nba articles from the {sportsite}.</p>
+<p>Optional params: limit returns the maximum number of articles desired</p>
+<p>Example /news/source/espn?limit=5</p>
+
+<h3>/news/source/{sportsite}</h3>
+<div>
+<p>Returns a list of all the latest nba articles from the {sportsite}.</p>
+<p>Optional params: limit returns the maximum number of articles desired</p>
+<p>Example /news/source/espn?limit=5</p>
+
+<h3>/news/player/{player name}</h3>
+<p>Returns a list of all the latest nba articles of{player name}.</p>
+<p>Optional params: limit returns the maximum number of articles desired</p>
+<p>Use dash to seperate names Searching by players full name seperated by dash produces best results Example /news/player/kevin-durant?limit=10</p>
+
+<h3>/news/team/{team-name}</h3>
+<p>Returns a list of all the latest nba articles of{team}.</p>
+<p>Optional params: limit returns the maximum number of articles desired</p>
+<p>Use dash to seperate names Searching by team name produces best results Example /news/team/raptors</p>
+`)
+);
 app.use('/news', newsRouter);
 app.use(middleware.unknownEndpoint);
 
